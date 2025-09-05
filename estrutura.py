4b11d46c35378a4a0eb3f4e61f861e037b453ae2
gabrielguarnieri007 <guarnieri99sousa@gmail.com>
2025-09-05 00:21:14 -0300

    feat: Implementa a classe Processo para armazenar dados

---
+++ estrutura.py
@@ -0,0 +1,11 @@
+class Processo:
+    def __init__(self, id: int, nome: str, prioridade: int, ciclos_necessarios: int, recurso_necessario: str = None):
+        self.id = id
+        self.nome = nome
+        self.prioridade = prioridade
+        self.ciclos_necessarios = ciclos_necessarios
+        self.recurso_necessario = recurso_necessario
+        self.prioridade_original = prioridade
+
+    def __str__(self):
+        return f"ID[{self.id}]-{self.nome}(P{self.prioridade}, {self.ciclos_necessarios} ciclos)"
